76772df6a0e1e307412e2fdca49288e72c496514
Mehmet Sutas <msutas@rigid3d.com>
2020-06-22 02:57:41 +0300

    Revert "Update server.py"
    This reverts commit ce793c7e8f752d15c61458d221043f1db1455b66.

--- netconnectd/server.py
+++ netconnectd/server.py
@@ -312,12 +312,13 @@ class Server(object):
 
         # make sure multicast addresses can be routed on the AP
         self.logger.debug("Removing multicast routes")
-        #try:
-        #    subprocess.check_output(['/sbin/ip', 'route', 'del', '224.0.0.0/4', 'dev', self.wifi_if])
-        #    subprocess.check_output(['/sbin/ip', 'route', 'del', '239.255.255.250', 'dev', self.wifi_if])
-        #except subprocess.CalledProcessError as e:
-        #    self.logger.exception("Could not remove multicast routes")
-        #    self.logger.warn("Output: %s" % e.output)
+        try:
+            subprocess.check_output(['/sbin/ip', 'route', 'del', '224.0.0.0/4', 'dev', self.wifi_if])
+            subprocess.check_output(['/sbin/ip', 'route', 'del', '239.255.255.250', 'dev', self.wifi_if])
+        except subprocess.CalledProcessError as e:
+            self.logger.exception("Could not remove multicast routes")
+            self.logger.warn("Output: %s" % e.output)
+            continue
 
         self.logger.debug("Freeing wifi interface")
         self.free_wifi()
